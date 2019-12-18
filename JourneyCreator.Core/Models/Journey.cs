67a8286db2f69411efe4a0fe6de87b6ad4126dd4
James <james.price@gocompare.com>
2019-12-18 18:37:20 +0000

    validate all the things

--- JourneyCreator.Core/Models/Journey.cs
+++ JourneyCreator.Core/Models/Journey.cs
@@ -4,10 +4,9 @@ namespace JourneyCreator.Core.Models
 {
     public class Journey
     {
-        // Todo: Add isTest
         public int Id { get; set; }
         public string Publisher { get; set; }
-        public int ProductId { get; set; }
+        public string Product { get; set; }
         public IEnumerable<Page> Pages { get; set; }
         public GTMSettings GTMSettings { get; set; }
     }
